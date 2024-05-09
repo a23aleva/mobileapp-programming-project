aad227880b854df1305a93aba878d88955c4d10e
a23aleva <a23aleva@student.his.se>
2024-05-09 20:27:54 +0200

    Creating and executing a JsonTask

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.example.networking.JsonTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        JsonTask task = new JsonTask(new JsonTask.JsonTaskListener() {
+            @Override
+            public void onPostExecute(String json) {
+                Log.d("json data", json);
+            }
+        });
+
+        task.execute("https://mobprog.webug.se/json-api?login=a23aleva");
     }
 
 }

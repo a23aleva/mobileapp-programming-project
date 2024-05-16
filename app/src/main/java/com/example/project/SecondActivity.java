e026d87feba1f9d52b9bcfe6755a12711240d852
a23aleva <a23aleva@student.his.se>
2024-05-16 21:09:25 +0200

    Added a html about file to show in internal WebView

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        WebView webView = findViewById(R.id.WebView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/About.html");
     }
 }
